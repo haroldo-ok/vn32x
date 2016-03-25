7baa244e54e97420157747541ae4182fecc57827
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-03-25 11:16:06 -0300

    Code cleanup
    Moved linebuffer setup into a function.
    Added constants for width and height.
    Removed some dead code.
    Removed some commented out code.

--- src/main.c
+++ src/main.c
@@ -5,11 +5,13 @@
 #include "32x.h"
 #include "aplib_decrunch.h"
 
+#define FBF_WIDTH 320
+#define FBF_HEIGHT 202
+
 extern vu16 maruko[], test[];
 int numColors;
 
-unsigned char temp_buffer[320 * 224];
-
+unsigned char temp_buffer[FBF_WIDTH * FBF_HEIGHT];
 
 void slave()
 {
@@ -31,19 +33,35 @@ void drawApgImage(int x, int y, vu16 *apg) {
 	for (i = 0; i != height; i++) {
 		for (j = 0; j != width; j++) {
 			if (temp_buffer[i * width + j] != transparency) {
-				frameBuffer16[(i + y) * 320 + (j + x) + 0x100] = pal[temp_buffer[i * width + j]];
+				frameBuffer16[(i + y) * FBF_WIDTH + (j + x) + 0x100] = pal[temp_buffer[i * width + j]];
 			}
 		}			
 	}
 }
 
+int setupLineTable() {
+	int i, lineOffs;
+	vu16 *frameBuffer16 = &MARS_FRAMEBUFFER;
+	
+	// Set up the line table
+	lineOffs = 0x100;
+	for (i = 11; i < 213; i++) {
+		frameBuffer16[i] = lineOffs;
+		lineOffs += FBF_WIDTH;
+	}
+
+	lineOffs = (FBF_HEIGHT + 1) * FBF_WIDTH;
+	for (i = 0; i < 11; i++) {
+		frameBuffer16[i] = lineOffs;
+	}
+	for (i = 213; i < 256; i++) {
+		frameBuffer16[i] = lineOffs;
+	}	
+}
 
 int main()
 {
 	uint16 currentFB=0;
-	uint16 lineOffs;
-	vu16 *frameBuffer16 = &MARS_FRAMEBUFFER;
-	unsigned char *frameBuffer8 = &MARS_FRAMEBUFFER;
 
 	int i, j, t;
 
@@ -53,15 +71,6 @@ int main()
 	// Set 8-bit paletted color mode, 224 lines
 	MARS_VDP_DISPMODE = MARS_224_LINES | MARS_VDP_MODE_32K;
 
-	/*
-	numColors = 128;
-	for (i = 0; i < numColors; i++)
-	{
-		cram16[i] = pal16[i] & 0x7FFF;
-		cram16[i + numColors] = pal16b[i] & 0x7FFF;
-	}
-	*/
-
 	MARS_VDP_FBCTL = currentFB;
 
     for(;;)
@@ -70,36 +79,13 @@ int main()
 		while ((MARS_VDP_FBCTL & MARS_VDP_FS) == currentFB) {}
 		currentFB ^= 1;
 
-		/*
-		aplib_decrunch(&maruko, temp_buffer);
-		
-		for (i = 0; i != 202; i++) {
-			for (j = 0; j != 320; j++) {
-				frameBuffer16[i * 320 + j + 0x100] = pal16[temp_buffer[i * 320 + j]];
-			}			
-		}
-		*/
-
 		drawApgImage(0, 0, maruko);
 		drawApgImage(t, 32, test);
 		
 		//frameBuffer16[t + 0x100] = t;
 		t++;
 
-		// Set up the line table
-		lineOffs = 0x100;
-		for (i = 11; i < 213; i++) {
-			frameBuffer16[i] = lineOffs;
-			lineOffs += 320;
-		}
-
-		lineOffs = 203 * 320;
-		for (i = 0; i < 11; i++) {
-			frameBuffer16[i] = lineOffs;
-		}
-		for (i = 213; i < 256; i++) {
-			frameBuffer16[i] = lineOffs;
-		}
+		setupLineTable();
      }
 
 	return 0;
