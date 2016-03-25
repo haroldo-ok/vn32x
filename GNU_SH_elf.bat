e45cc8ad316d84e00fe3ba9003bb7f63fb3a21c6
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-03-25 10:51:27 -0300

    APG generation added to the build process

--- GNU_SH_elf.bat
+++ GNU_SH_elf.bat
@@ -3,8 +3,9 @@
 @REM Set this according to your own environment setup
 SET DEVKITROOT=C:\Util\dev32x
 SET PACKFIREROOT=C:\Util\sixpack-13
+SET TOOLROOT=%~dp0\tool
 
-Set PATH=%DEVKITROOT%\bin;%DEVKITROOT%\sh-elf\bin;%DEVKITROOT%\m68k-elf\bin;%DEVKITROOT%\UTILS;%PACKFIREROOT%\bin;%PATH%
+Set PATH=%DEVKITROOT%\bin;%DEVKITROOT%\sh-elf\bin;%DEVKITROOT%\m68k-elf\bin;%DEVKITROOT%\UTILS;%PACKFIREROOT%\bin;%TOOLROOT%\bin;%PATH%
 Set MAKE_MODE=unix
 
 Cmd "cd\" /k
