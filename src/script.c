107ef291af99ebb21bfb9b38103bea973f5632d1
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-05-04 20:59:28 -0300

    Tested script branching

--- src/script.c
+++ src/script.c
@@ -2,15 +2,32 @@
 #include "vn_engine.h"
 #include "script.h"
 
+extern void *vn_first_option();
+extern void *vn_second_option();
+
 void *vn_start() {
 	vnText("Here's some text. It's so long, it spans multiple lines. Testing word wrapping.\nLine breaks are supported, too.\nAlso, it can span multiple pages, if so required.");
 
 	initMenu();
-	addMenuItem("Option one");
-	addMenuItem("Option two");
-	addMenuItem("Option three");
-	vnMenu();
+	int option1 = addMenuItem("Option one");
+	int option2 = addMenuItem("Option two");
+	int chosenOption = vnMenu();
+	
+	if (chosenOption == option1) {
+		return vn_first_option;		
+	} else if (chosenOption == option2) {
+		return vn_second_option;		
+	}
 
 	return vn_start;
 }
 
+void *vn_first_option() {
+	vnText("You chose the first option");
+	return vn_start;
+}
+
+void *vn_second_option() {
+	vnText("You chose the second option");
+	return vn_start;
+}
