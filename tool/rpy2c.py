c6bb25bf4e57328d76f0ebfd0cb027ef55d99f14
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-05-16 23:18:47 -0300

    Added sources from Ren'py so they can be modified

--- tool/rpy2c.py
+++ tool/rpy2c.py
@@ -1,16 +1,16 @@
 import sys, os
 
-import pyximport
-pyximport.install()
-
-renpy_home = 'C:/Util/renpy-6.99.10-sdk'
-sys.path.append(renpy_home)
+#renpy_home = 'C:/Util/renpy-6.99.10-sdk'
+sys.path.append(r'C:\Users\Haroldo\Projetos\32x\vn32x\tool')
 
 def main ():
-    import renpy
+    #import renpy
+    """
     import renpy.object
     import renpy.exports
     import renpy.config
+    import renpy.game
+    """
     import renpy.parser
     renpy.parser.parse(r'C:\Util\renpy-6.99.10-sdk\the_question\game\script.rpy')
 
