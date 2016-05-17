672def1c949bca81f385b88c7600ea0b0ea766f1
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-05-16 23:02:59 -0300

    Having problems with cython dependencies.

--- tool/rpy2c.py
+++ tool/rpy2c.py
@@ -1,10 +1,18 @@
 import sys, os
 
+import pyximport
+pyximport.install()
+
 renpy_home = 'C:/Util/renpy-6.99.10-sdk'
 sys.path.append(renpy_home)
 
 def main ():
+    import renpy
+    import renpy.object
+    import renpy.exports
+    import renpy.config
     import renpy.parser
     renpy.parser.parse(r'C:\Util\renpy-6.99.10-sdk\the_question\game\script.rpy')
+
 
 main()
