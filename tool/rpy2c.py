0f2308a4bd860b165f48bd8378c62dd55a12eaa0
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-05-17 22:11:34 -0300

    Starting from scratch, this time via Ply

--- tool/rpy2c.py
+++ tool/rpy2c.py
@@ -1,23 +1,3 @@
 import sys, os
 
-sys.path.append(r'C:\Users\Haroldo\Projetos\32x\vn32x\tool')
-
-def main ():
-    #import renpy
-    """
-    import renpy.object
-    import renpy.exports
-    import renpy.config
-    import renpy.game
-    """
-    import renpy.parser
-    import renpy.game
-    import renpy.execution
-
-    context = renpy.execution.Context(False, None, clear=True)
-    renpy.game.contexts.append(context)
-
-    renpy.parser.parse(r'C:\Util\renpy-6.99.10-sdk\the_question\game\script.rpy')
-
-
-main()
+print r'C:\Util\renpy-6.99.10-sdk\the_question\game\script.rpy'
