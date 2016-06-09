be76b2b1f12cbb43076e9b5db7a7bae8ee56b624
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-06-09 17:15:32 -0300

    Created main script; problem parsing.

---
+++ script/script.rpy
@@ -0,0 +1,4 @@
+label start:
+	"Well, professor Eileen's lecture was interesting."
+	"Sylvie" "Oh, hi, do we walk home together?"
+	m "Yes..."
---
+++ tool/rpy2c/rpy2c.py
@@ -0,0 +1,16 @@
+import sys, argparse, os
+import rpy_parser
+
+def main (argv):
+    parser = argparse.ArgumentParser(description="Convert Ren'py scripts into C code.")
+    parser.add_argument("source_rpy")
+    parser.add_argument("destination_folder")
+    opts = parser.parse_args()
+
+    with open(os.path.abspath(opts.source_rpy), 'rt') as f:
+        source = ''.join(f.readlines())
+        tree = rpy_parser.RpyParser().parse(source)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
