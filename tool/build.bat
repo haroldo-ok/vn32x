710c1fb862ccf21c6fef297391b31740eb72f343
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-06-12 16:24:47 -0300

    Generate rpy2c binaries

--- tool/build.bat
+++ tool/build.bat
@@ -2,5 +2,6 @@ SETLOCAL
 set PYTHON_HOME=c:\Python27
 mkdir bin
 del /q bin\*
-call %PYTHON_HOME%\Scripts\cxfreeze apg.py --target-dir bin --include-path=tw;lib
-call %PYTHON_HOME%\Scripts\cxfreeze font_conv.py --target-dir bin --include-path=tw;lib
+call %PYTHON_HOME%\Scripts\cxfreeze apg.py --target-dir bin 
+call %PYTHON_HOME%\Scripts\cxfreeze font_conv.py --target-dir bin 
+call %PYTHON_HOME%\Scripts\cxfreeze rpy2c/rpy2c.py --target-dir bin 
