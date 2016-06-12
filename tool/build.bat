05e669da3ddd707f90334b316fd1127b353a4243
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-06-12 16:54:54 -0300

    ply2c.exe was complaining missing 'lex' library.

--- tool/build.bat
+++ tool/build.bat
@@ -4,4 +4,4 @@ mkdir bin
 del /q bin\*
 call %PYTHON_HOME%\Scripts\cxfreeze apg.py --target-dir bin 
 call %PYTHON_HOME%\Scripts\cxfreeze font_conv.py --target-dir bin 
-call %PYTHON_HOME%\Scripts\cxfreeze rpy2c/rpy2c.py --target-dir bin 
+call %PYTHON_HOME%\Scripts\cxfreeze rpy2c/rpy2c.py --target-dir bin
