84279bdfc70f94147ff30082b2191c1218330706
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-10-10 19:56:29 -0300

    Word alignment for the base images.

--- src/image.s
+++ src/image.s
@@ -6,16 +6,21 @@
 .globl _default_font
 
 _bedday:
+.align 2
 .incbin "build/bedday.apg"
 
 _pose:
+.align 2
 .incbin "build/pose.apg"
 
 _text_frame:
+.align 2
 .incbin "build/text_frame.apg"
 
 _next_page_icon:
+.align 2
 .incbin "build/down.apg"
 
 _default_font:
+.align 2
 .incbin "build/font_dejavu_sans.bmf"
