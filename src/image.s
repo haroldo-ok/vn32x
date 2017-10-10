b55a9377a373050642c4e177b127f36775b6829e
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-10-10 20:37:23 -0300

    Removed unnecessary resources from the compilation

--- src/image.s
+++ src/image.s
@@ -1,26 +1,17 @@
 .text
+.align 2
+
 .globl _bedday
 .globl _pose
 .globl _text_frame
 .globl _next_page_icon
 .globl _default_font
 
-_bedday:
-.align 2
-.incbin "build/bedday.apg"
-
-_pose:
-.align 2
-.incbin "build/pose.apg"
-
 _text_frame:
-.align 2
 .incbin "build/text_frame.apg"
 
 _next_page_icon:
-.align 2
 .incbin "build/down.apg"
 
 _default_font:
-.align 2
 .incbin "build/font_dejavu_sans.bmf"
