62760bae75d1813d6f88f844223e6d5522f73b59
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-10-12 19:05:02 -0300

    Improved the data alignment.

--- src/image.s
+++ src/image.s
@@ -1,5 +1,4 @@
 .text
-.align 2
 
 .globl _bedday
 .globl _pose
@@ -7,11 +6,17 @@
 .globl _next_page_icon
 .globl _default_font
 
+.align 2
+.word 0
 _text_frame:
 .incbin "build/text_frame.apg"
 
+.align 2
+.word 0
 _next_page_icon:
 .incbin "build/down.apg"
 
+.align 2
+.word 0
 _default_font:
 .incbin "build/font_dejavu_sans.bmf"
